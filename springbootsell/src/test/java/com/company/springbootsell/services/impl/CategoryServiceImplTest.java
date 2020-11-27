4184e855b553bc7d298a368c8d4edabf504c0df9
linlinma <2377809209@qq.com>
2020-11-27 17:22:30 +0800

    111

--- springbootsell/src/test/java/com/company/springbootsell/services/impl/CategoryServiceImplTest.java
+++ springbootsell/src/test/java/com/company/springbootsell/services/impl/CategoryServiceImplTest.java
@@ -21,14 +21,17 @@ public class CategoryServiceImplTest {
         ProductCategory productCategory=categoryService.findOne(3);
         System.out.println(productCategory);
         Assert.assertEquals(new Integer(3),productCategory.getCategoryId());
+
     }
 
     @Test
     public void findAll(){
         List<ProductCategory> list=categoryService.findAll();
+        //for 增强遍历循环， 返回对象类型+对象名+集合  程序
         for (ProductCategory category:list){
             System.out.println(category);
             Assert.assertNotEquals(0,list.size());
+            //assertNotEquals 得到数组不为0  测试通过
         }
     }
    @Test
